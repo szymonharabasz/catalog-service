d39ef041b0f21a7499691290db5fcb90827c1b9c
Szymon Harabasz <szymon.harabasz@gmail.com>
2023-04-16 14:07:53 +0200

    Added a short delay before starting the app in order to have the Config Server available when running with Docker Compose

--- src/main/java/com/polarbookshop/catalogservice/CatalogServiceApplication.java
+++ src/main/java/com/polarbookshop/catalogservice/CatalogServiceApplication.java
@@ -1,5 +1,7 @@
 package com.polarbookshop.catalogservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
@@ -9,6 +11,12 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 public class CatalogServiceApplication {
 
 	public static void main(String[] args) {
+		int secondsToSleep = 5;
+		try {	
+			Thread.sleep(secondsToSleep * 1000);
+		} catch (InterruptedException ie) {
+			Thread.currentThread().interrupt();
+		}
 		SpringApplication.run(CatalogServiceApplication.class, args);
 	}
 
